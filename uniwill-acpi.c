a4c36a3239e0e69dc895c800a803ae152fc367e3
Andrey Trishin <zatrit@gmail.com>
2026-03-07 20:08:53 +0300

    Add initial MACHENIKE L16P support
    Adds a DMI entry for the MACHENIKE L16P and enables tested EC features.
    
    Signed-off-by: Andrey Trishin <zatrit@gmail.com>

--- uniwill-acpi.c
+++ uniwill-acpi.c
@@ -2609,7 +2609,28 @@ static struct uniwill_device_descriptor pf5pu1g_descriptor __initdata = {
 		    UNIWILL_FEATURE_PRIMARY_FAN,
 };
 
+static struct uniwill_device_descriptor machenike_l16p_descriptor __initdata = {
+	.features = UNIWILL_FEATURE_FN_LOCK |
+        UNIWILL_FEATURE_SUPER_KEY |
+        UNIWILL_FEATURE_CPU_TEMP |
+        UNIWILL_FEATURE_GPU_TEMP |
+        UNIWILL_FEATURE_PRIMARY_FAN |
+        UNIWILL_FEATURE_SECONDARY_FAN |
+        UNIWILL_FEATURE_BATTERY_CHARGE_LIMIT |
+        UNIWILL_FEATURE_AC_AUTO_BOOT |
+        UNIWILL_FEATURE_USB_POWERSHARE |
+        UNIWILL_FEATURE_KEYBOARD_BACKLIGHT,
+};
+
 static const struct dmi_system_id uniwill_dmi_table[] __initconst = {
+	{
+		.ident = "MACHENIKE L16 Pro",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "MACHENIKE"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "L16P"),
+		},
+		.driver_data = &machenike_l16p_descriptor,
+	},
 	{
 		.ident = "XMG FUSION 15 (L19)",
 		.matches = {
